6f3138f080af3fbdfc6cf915c1f5343e2c886b48
Ravi <42536488+ravipurohit1991@users.noreply.github.com>
2022-11-17 00:19:43 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,12 @@ go_to_build_dir() {
     fi
 }
 
+go_to_build_dir_all() {
+    if [ ! -z $INPUT_SUBDIRALL ]; then
+        cd $INPUT_SUBDIRALL
+    fi
+}
+
 check_if_meta_yaml_file_exists() {
     if [ ! -f meta.yaml ]; then
         echo "meta.yaml must exist in the directory that is being packaged and published."
@@ -17,15 +23,34 @@ check_if_meta_yaml_file_exists() {
 }
 
 build_package(){
-    conda build -c conda-forge -c bioconda --output-folder . .
+    conda build --output --check -c conda-forge --output-folder . .
+    conda convert -p osx-64 linux-64/*.tar.bz2
+    conda convert -p win-64 linux-64/*.tar.bz2
 }
 
 upload_package(){
     export ANACONDA_API_TOKEN=$INPUT_ANACONDATOKEN
+    anaconda upload --label main linux-64/*.tar.bz2
+    anaconda upload --label main osx-64/*.tar.bz2
+    anaconda upload --label main win-64/*.tar.bz2
+}
+
+build_package_noarch(){
+    conda build --output --check -c conda-forge --output-folder . .
+}
+
+upload_package_noarch(){
+    export ANACONDA_API_TOKEN=$INPUT_ANACONDATOKEN
     anaconda upload --label main noarch/*.tar.bz2
 }
+
 
 go_to_build_dir
 check_if_meta_yaml_file_exists
 build_package
 upload_package
+
+go_to_build_dir_all
+check_if_meta_yaml_file_exists
+build_package_noarch
+upload_package_noarch
